45403d08c40f2764e43a74abfd048dc3384d0a7f
aleckinler <aleckinler@gmail.com>
2022-01-20 21:31:34 -0600

    shows both comment systems

--- classwork/MovieLibrary/MovieLib.ConsoleHost/Program.cs
+++ classwork/MovieLibrary/MovieLib.ConsoleHost/Program.cs
@@ -1,13 +1,21 @@
-﻿using System;
+﻿/* FILE HEADER EXAMPLE
+ * Your Name
+ * ITSE 1430
+ * EXAMPLE LAB
+ */
+using System;
 
 namespace MovieLib.ConsoleHost
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main ( string[] args )
         {
             //if there were 2 astronauts on the moon and one killed the other with like a moon rock that would be pretty messed up
             Console.WriteLine("Hello World!");
+
+            /*wooooah dude
+             * this is bananas my man*/
         }
     }
 }
