dda408d955d8d84d001e274c52af359792da0d39
aleckinler <aleckinler@gmail.com>
2022-01-25 19:52:14 -0600

    character creator lab

---
+++ labs/Lab1/CharacterCreator.ConsoleHost/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CharacterCreator.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
